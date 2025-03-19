22a65585e150d70c32d6b3d240ca2380023d6601
Olaxy13 <olaxy13@gmail.com>
2025-03-19 02:23:12 -0700

    Working

--- trial/dto/update-trial.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateTrialDto } from './create-trial.dto';
-
-export class UpdateTrialDto extends PartialType(CreateTrialDto) {}
--- trial/trial.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { TrialService } from './trial.service';
-import { TrialController } from './trial.controller';
-
-@Module({
-  controllers: [TrialController],
-  providers: [TrialService],
-})
-export class TrialModule {}
